3cbfdd5ea7d15efebdc478c58d87912813c2f42a
Mariana Duran <marianaduran260@gmail.com>
2026-03-24 23:59:55 -0700

    yeth

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -13,7 +13,21 @@ public class KitchenObject : MonoBehaviour
 
     public void SetClearCounter(ClearCounter clearCounter)
     {
+        if(this.clearCounter  != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Counter Already Has A KitchenObject!");
+        }
+
         this.clearCounter = clearCounter;
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
     }
 
     public ClearCounter GetClearCounter()
